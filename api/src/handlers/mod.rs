79a59f728b24fb1c8cf44b17a7c6d67c5b6e3330
Tommie Nygren <tommie.nygren@gmail.com>
2025-06-10 15:46:22 +0200

    Adds some files to the module, but more is needed. Sync the ENTITIES!

--- api/src/handlers/mod.rs
+++ api/src/handlers/mod.rs
@@ -1,7 +1,8 @@
-pub(crate) mod posts;
+pub(crate) mod auth;
+pub(crate) mod budget;
+pub(crate) mod budget_plan;
+pub(crate) mod import;
 pub(crate) mod index;
 pub(crate) mod members;
 pub mod open_id_connect;
-pub(crate) mod auth;
-pub(crate) mod import;
-
+pub(crate) mod posts;
