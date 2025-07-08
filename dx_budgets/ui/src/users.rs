de229ec0f781a75fd66a4771e7d297a492683448
Tommie Nygren <tommie.nygren@gmail.com>
2025-07-08 15:53:57 +0200

    OK; so now we have actually found how to do it "properly" (maybe).

--- dx_budgets/ui/src/users.rs
+++ dx_budgets/ui/src/users.rs
@@ -2,25 +2,16 @@ use dioxus::prelude::*;
 
 const ECHO_CSS: Asset = asset!("/assets/styling/echo.css");
 
-/// Echo component that demonstrates fullstack server functions.
 #[component]
 pub fn Users() -> Element {
-    let users = use_resource(api::list_users).suspend()?;
-
-    let users_2 = use_server_future(|| api::list_users())?;
-    let users_2 = users_2().unwrap();
+    let users_2 = use_server_future(|| api::list_users())?().unwrap().unwrap_or_default();
     rsx! {
         document::Link { rel: "stylesheet", href: ECHO_CSS }
         div {
             id: "echo",
             h4 { "Users, bro" }
             ul {
-                    for user in users().unwrap().iter() {
-                        li { key: "{user.id}", "{user.first_name} {user.last_name}" }
-                    }
-            }
-            ul {
-                for user in users_2.unwrap().iter() {
+                for user in users_2.iter() {
                     li { key: "{user.id}", "{user.first_name} {user.last_name}" }
                 }
             }
