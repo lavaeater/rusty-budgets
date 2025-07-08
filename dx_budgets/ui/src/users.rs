17018179660300da2db26983ddf5d184673900d8
Tommie Nygren <tommie.nygren@gmail.com>
2025-07-08 16:05:40 +0200

    Styling.

--- dx_budgets/ui/src/users.rs
+++ dx_budgets/ui/src/users.rs
@@ -5,15 +5,27 @@ const ECHO_CSS: Asset = asset!("/assets/styling/echo.css");
 
 #[component]
 pub fn Users() -> Element {
-    let users_2 = use_server_future(|| api::list_users())?().unwrap().unwrap_or_default();
+    let users = use_server_future(|| api::list_users())?().unwrap().unwrap_or_default();
     rsx! {
         document::Link { rel: "stylesheet", href: ECHO_CSS }
         div {
-            id: "echo",
+            id: "users_list",
             h4 { "Users, bro" }
-            ul {
-                for user in users_2.iter() {
-                    li { key: "{user.id}", "{user.first_name} {user.last_name}" }
+            table {
+                thead {
+                    tr {
+                        th { "First Name" }
+                        th { "Last Name" }
+                    }
+                }
+                tbody {
+                    for user in users.iter() {
+                        tr {
+                            key: "{user.id}",
+                            td { "{user.first_name}" }
+                            td { "{user.last_name}" }
+                        }
+                    }
                 }
             }
         }
