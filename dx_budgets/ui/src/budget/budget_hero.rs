4d067f70724eb0a911fde5460d07af24babecb44
Tommie Nygren <tommie.nygren@gmail.com>
2025-07-12 14:03:24 +0200

    Downgraded, god damned!

--- dx_budgets/ui/src/budget/budget_hero.rs
+++ dx_budgets/ui/src/budget/budget_hero.rs
@@ -5,12 +5,25 @@ const BUDGET_CSS: Asset = asset!("/assets/styling/budget.css");
 
 #[component]
 pub fn BudgetHero() -> Element {
-    let budgets = use_server_future(|| api::get_default_budget())?().unwrap().unwrap();
+    let mut edit_budget_name = use_signal(|| false);
+    let budget = use_server_future(|| api::get_default_budget())?().unwrap()?;
     rsx! {
         document::Link { rel: "stylesheet", href: BUDGET_CSS }
         div {
-            id: "budget_hero",
-            h4 { "{budgets.name}" }
+            id: "budget_hero"
+            if edit_budget_name {
+                input {
+                    type: "text",
+                    value: "{budget.name}",
+                }
+            } else {
+                h4 {
+                    "{budget.name}",
+                    onclick: move |_| {
+                        edit_budget_name.set(true);
+                    }
+                }
+            }
         }
     }
 }
