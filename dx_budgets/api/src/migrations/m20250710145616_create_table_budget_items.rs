6ef1f54347a14e04668ff7e44f597f4207f7d8d8
Tommie Nygren <tommie.nygren@gmail.com>
2025-07-22 10:27:18 +0200

    Yay

--- dx_budgets/api/src/migrations/m20250710145616_create_table_budget_items.rs
+++ dx_budgets/api/src/migrations/m20250710145616_create_table_budget_items.rs
@@ -8,7 +8,8 @@ pub(super) fn step(_state: &TableState) -> Result<MigrationStep> {
         .column(|c| c("name", Type::String))
         .column(|c| c("amount", Type::Float))
         .column(|c| {
-            c("budget_id", Type::Uuid).create_foreign_key("budgets", "id", OnDelete::Cascade)
+            c("budget_id", Type::Uuid)
+                .create_foreign_key("budgets", "id", OnDelete::Cascade)
         });
     Ok(MigrationStep::new(
         "m20250710145616_create_table_budget_items",
