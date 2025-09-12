c62aaf0d5d5a6b4458d923e1f6bc71c1b69d2dd7
Tommie Nygren <tommie.nygren@gmail.com>
2025-09-12 09:59:18 +0200

    Fucking select box

--- ui/src/budget/budget_group_view.rs
+++ ui/src/budget/budget_group_view.rs
@@ -1,7 +1,9 @@
 use dioxus::logger::tracing;
 use dioxus::prelude::*;
 use dioxus_primitives::accordion::{AccordionContent, AccordionItem, AccordionTrigger};
-use api::cqrs::budget::BudgetGroup;
+use dioxus_primitives::select::*;
+use api::cqrs::budget::{BudgetGroup, BudgetItemType};
+use api::cqrs::money::{Currency, Money};
 
 const BUDGET_CSS: Asset = asset!("/assets/styling/budget.css");
 
@@ -9,7 +11,8 @@ const BUDGET_CSS: Asset = asset!("/assets/styling/budget.css");
 pub fn BudgetGroupView(group: BudgetGroup, index: usize) -> Element {
     let mut show_new_item = use_signal(|| false);
     let mut new_item_name = use_signal(|| "".to_string());
-    let mut new_item_amount = use_signal(|| Money::new(0, "SEK"));
+    let mut new_item_amount = use_signal(|| Money::new(0, Currency::SEK));
+    let mut new_item_type = use_signal(|| Some(Some(BudgetItemType::Income)));
     
     rsx! {
         AccordionItem {
@@ -42,13 +45,15 @@ pub fn BudgetGroupView(group: BudgetGroup, index: usize) -> Element {
                                 r#type: "text",
                                 placeholder: "Budgetpostnamn",
                                 oninput: move |e| { new_item_name.set(e.value()) },
-                            
                             }
                             input {
                                 r#type: "number",
                                 placeholder: "Budgetpostbelopp",
-                                oninput: move |e| { new_item_amount.set(Money::new(e.value().parse().unwrap(), "SEK")) },
+                                oninput: move |e| {
+                                    new_item_amount.set(Money::new(e.value().parse().unwrap(), Currency::SEK))
+                                },
                             }
+                            ItemTypeSelect { selected_value: new_item_type }
                         }
                     } else {
                         button {
@@ -60,6 +65,50 @@ pub fn BudgetGroupView(group: BudgetGroup, index: usize) -> Element {
                             "Ny budgetpost"
                         }
                     }
+                }
+            }
+        }
+    }
+}
+
+#[component]
+pub fn ItemTypeSelect(mut selected_value: Signal<Option<Option<BudgetItemType>>>) -> Element {
+    rsx! {
+        Select::<BudgetItemType> {
+            // The currently selected value(s) in the dropdown.
+            value: selected_value,
+            // Callback function triggered when the selected value changes.
+            on_value_change: move |value: Option<BudgetItemType>| {
+                selected_value.set(Some(value));
+                if let Some(val) = value {
+                    tracing::info!("Selected value: {:?}", val);
+                }
+            },
+            // The select trigger is the button that opens the dropdown.
+            SelectTrigger {
+                // The (optional) select value displays the currently selected text value.
+                SelectValue {}
+            }
+            // All groups must be wrapped in the select list.
+            SelectList {
+                // Each select option represents an individual option in the dropdown. The type must match the type of the select.
+                SelectOption::<BudgetItemType> {
+                    index: 0usize,
+                    // The value of the item, which will be passed to the on_value_change callback when selected.
+                    value: BudgetItemType::Income,
+                    text_value: "Inkomst",
+                }
+                SelectOption::<BudgetItemType> {
+                    index: 1usize,
+                    // The value of the item, which will be passed to the on_value_change callback when selected.
+                    value: BudgetItemType::Expense,
+                    text_value: "Utgift",
+                }
+                SelectOption::<BudgetItemType> {
+                    index: 2usize,
+                    // The value of the item, which will be passed to the on_value_change callback when selected.
+                    value: BudgetItemType::Savings,
+                    text_value: "Sparande",
                 }
             }
         }
