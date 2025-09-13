0d78e4ffd4add4d5f9369ebb3bce396ce1bd9686
Tommie Nygren <tommie.nygren@gmail.com>
2025-09-13 14:30:32 +0200

    This is fucking crazy

--- ui/src/budget/budget_item_view.rs
+++ ui/src/budget/budget_item_view.rs
@@ -1,11 +1,20 @@
 use api::cqrs::budget::BudgetItem;
 use dioxus::prelude::*;
-
+use dioxus::logger::tracing;
 #[component]
 pub fn BudgetItemView(item: BudgetItem, index: usize) -> Element {
+    tracing::info!("item_name: {}", item.name);
+    tracing::info!("item_type: {}", item.item_type);
+    tracing::info!("item_amount: {}", item.budgeted_amount.to_string());
+    
+    let item_amount = use_signal(|| item.budgeted_amount.to_string());
+    let item_type = use_signal(|| item.item_type.to_string());
+    
     rsx! {
-        h3 { {item.name} }
-        h3 { {item.item_type.to_string()} }
-        h3 { {item.budgeted_amount.to_string()} }
+        div {
+            h3 { {item.name} }
+            p { {item_type} }
+            p { {item_amount} }
+        }
     }
 }
