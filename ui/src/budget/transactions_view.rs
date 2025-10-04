1a34ff0621ce2b656ef5ff024dfae98d2db03942
Tommie Nygren <tommie.nygren@gmail.com>
2025-10-04 11:01:26 +0200

    What is even happening?

--- ui/src/budget/transactions_view.rs
+++ ui/src/budget/transactions_view.rs
@@ -1,21 +1,22 @@
 use crate::budget::BudgetingTypeCard;
+use crate::budget::BudgetingTypeOverviewView;
 use crate::components::{TabContent, TabList, TabTrigger, Tabs};
 use api::models::{BankTransaction, Budget, BudgetItem, BudgetingType, BudgetingTypeOverview};
 use dioxus::prelude::*;
 use uuid::Uuid;
-use crate::budget::BudgetingTypeOverviewView;
 
 #[component]
-pub fn TransactionsView(
-    budget_id: Uuid,
-    transactions: Vec<BankTransaction>,
-) -> Element {
-    let  transacations = transactions.iter().filter(|tx| tx.budget_item_id.is_some()).collect::<Vec<_>>().sort_by_key(|tx| tx.date);
+pub fn TransactionsView(budget_id: Uuid, transactions: Vec<BankTransaction>) -> Element {
+    let transacations = transactions
+        .iter()
+        .filter(|tx| tx.budget_item_id.is_none())
+        .collect::<Vec<_>>()
+        .sort_by_key(|tx| tx.date);
     let no_item = use_signal(|| transactions);
-    
+
     rsx! {
         h1 { "Transactions" }
-        h2 { { no_item().len().to_string() }}
+        h2 { {no_item().len().to_string()} }
         div { flex: "row",
             div {
                 for transaction in no_item() {
