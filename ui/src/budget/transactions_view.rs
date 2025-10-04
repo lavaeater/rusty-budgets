64c4412ff8342fa8e4aa3a1dfb240dce0c202c7e
Tommie Nygren <tommie.nygren@gmail.com>
2025-10-04 10:58:13 +0200

    Started work on updatin' the transactions, mate!

--- ui/src/budget/transactions_view.rs
+++ ui/src/budget/transactions_view.rs
@@ -10,12 +10,15 @@ pub fn TransactionsView(
     budget_id: Uuid,
     transactions: Vec<BankTransaction>,
 ) -> Element {
+    let  transacations = transactions.iter().filter(|tx| tx.budget_item_id.is_some()).collect::<Vec<_>>().sort_by_key(|tx| tx.date);
+    let no_item = use_signal(|| transactions);
+    
     rsx! {
         h1 { "Transactions" }
-        h2 { { transactions.len().to_string() }}
+        h2 { { no_item().len().to_string() }}
         div { flex: "row",
             div {
-                for transaction in transactions {
+                for transaction in no_item() {
                     p { {transaction.description} }
                     p { {transaction.amount.to_string()} }
                     p { {transaction.date.to_string()} }
