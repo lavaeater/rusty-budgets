089df54fbc874b6dc6a1cf29b06e044d832aad0f
Tommie Nygren <tommie.nygren@gmail.com>
2025-11-07 10:00:27 +0100

    Updated itemadded with the refcell stuff.

--- api/src/events/item_added.rs
+++ api/src/events/item_added.rs
@@ -1,4 +1,5 @@
-use std::sync::{Arc, Mutex};
+use std::cell::RefCell;
+use std::rc::Rc;
 use crate::cqrs::framework::{Aggregate, CommandError, DomainEvent};
 use cqrs_macros::DomainEvent;
 use serde::{Deserialize, Serialize};
@@ -23,8 +24,8 @@ impl ItemAddedHandler for Budget {
             event.item_type,
         );
         
-        let the_arc_mutex = Arc::new(Mutex::new(new_item));
-        self.budget_items.insert(event.item_id, the_arc_mutex.clone());
+        let the_cell = Rc::new(RefCell::new(new_item));
+        self.budget_items.insert(event.item_id, the_cell.clone());
         
         event.item_id
     }
