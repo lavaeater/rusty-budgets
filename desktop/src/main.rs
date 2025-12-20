a6641e200397620be60f79492f6335be2c27a753
Tommie Nygren <tommie.nygren@gmail.com>
2025-12-20 16:33:07 +0100

    Harmonized desktop with the web thing so they work...

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -18,8 +18,8 @@ enum Route {
 
 fn main() {
     dioxus::logger::init(Level::INFO).expect("failed to init logger");
-    #[cfg(not(feature = "server"))]
-    fullstack::set_server_url("http://localhost");    
+    // #[cfg(not(feature = "server"))]
+    // fullstack::set_server_url("http://localhost");    
     #[cfg(feature = "server")]
     let _ = api::db::CLIENT;
     
