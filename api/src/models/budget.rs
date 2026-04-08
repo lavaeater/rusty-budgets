1d0dbe40462daf72d6f5a856dc7663d252845212
Tommie Nygren <tommie.nygren@gmail.com>
2026-04-08 17:48:07 +0200

    Yada yada.

--- api/src/models/budget.rs
+++ api/src/models/budget.rs
@@ -39,6 +39,7 @@ pub_events_enum! {
         TagCreated,
         TagModified,
         TransactionTagged,
+        TransactionUntagged,
         RuleModified,
         RuleDeleted,
         ItemBufferSet,
